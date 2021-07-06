1e36a8f097f5be0d88900863af6a89fca25eec83
Collin Sudik <Csudik@yahoo.com>
2021-07-06 19:47:59 -0400

    restarted countdown timer/new contact form

--- script.js
+++ script.js
@@ -16,56 +16,88 @@ button.onclick = function() {
 }
 
 // Countdown timer
-var currentDay;
-var time;
-var countdown;
+function dateDifference() {
+    const now = new Date();
+    const then = new Date("7/11/2021 14:00")
+    return then - now;
+} 
 
-function getSeconds() {
-    var nowDate = new Date();
-    var days = 6;
-    var timer = new
-    Date(nowDate.getFullYear(),nowDate.getMonth(),nowDate.getDate(),14,0,0);
-
-    var currentTime = nowDate.getTime();
-    var countdownTime = timer.getTime();
-    var diff = parseInt((countdownTime - currentTime)/1000)
-        if (diff < 0) {
-            currentDay = days - nowDate.getDay();
-        } else { currentDay = days - nowDate.getDay() -1 }
-        if (currentDay > 0) {
-            currentDay +=7;
-        } if (diff <=0) { diff += (86400 *7) }
-        startTimer (diff);
-        }
-
-function startTimer(secs) {
-    time = parseInt(secs);
-    countdown = setInterval("tick()", 1000);
-    tick();
+function days(seconds) {
+    return Math.floor(seconds/86400)
 }
 
-var secs;
+function hours(seconds) {
+    return Math.floor(seconds/3600)
+}
+
+function minutes(seconds) {
+    return Math.floor(seconds/60)
+}
 
 function tick() {
-    var secs = time;
-    if (secs > 0) {
-        time--;
-    } else {
-        clearInterval(countdown);
-        getSeconds();
-    }
+    let days = dateDifference()
+    document.getElementById("timer").innerHTML = days + ":" hours + ":" + minutes + ":" + seconds; //template literal?
 }
 
-var days = Math.floor(secs/86400);
-secs %= 86400;
-var hours = Math.floor(secs/3600);
-secs %= 3600;
-var mins = Math.floor(secs/60);
-secs %= 60;
+// var currentDay;
+// var time;
+// var countdown;
+
+// function getSeconds() {
+//     var nowDate = new Date();
+//     var days = 6;
+//     var timer = new
+//     Date(nowDate.getFullYear(),nowDate.getMonth(),nowDate.getDate(),14,0,0);
+
+//     var currentTime = nowDate.getTime();
+//     var countdownTime = timer.getTime();
+//     var diff = parseInt((countdownTime - currentTime)/1000)
+//     if (diff < 0) {
+//         currentDay = days - nowDate.getDay();
+//     } else { currentDay = days - nowDate.getDay() -1 }
+//     if (currentDay > 0) {
+//         currentDay +=7;
+//     } if (diff <=0) {
+//         diff += (86400 *7)
+//     }
+//     return diff;
+// }
+
+// function startTimer(secs) {
+//     time = parseInt(secs);
+//     countdown = setInterval(tick, 1000);
+//     tick();
+// }
+
+// var secs;
+
+// function tick() {
+//     var secs = time;
+//     if (secs > 0) {
+//         time--;
+//     } else {
+//         clearInterval(countdown);
+//     }
+//     var now = new Date();
+//     now.setSeconds(now.getSeconds()-getSeconds())
+    
+//     var formattedDate = new Intl.DateTimeFormat("en-US", { hour:"numeric", minute:"numeric", second:"numeric"}).format(now);
+//     document.getElementById("timer").innerHTML = formattedDate;
+// }
+
+// var days = Math.floor(secs/86400);
+// secs %= 86400;
+// var hours = Math.floor(secs/3600);
+// secs %= 3600;
+// var mins = Math.floor(secs/60);
+// secs %= 60;
 
 window.onload = function () {
-    document.getElementById("timer").innerHTML = getSeconds();
+    console.log(dateDifference());
 }
+//     startTimer (getSeconds());
+//     document.getElementById("timer").innerHTML = getSeconds();
+// }
 
 // function timer(duration, display) {
 //     var timer = duration, hours, minutes, seconds;
@@ -78,7 +110,7 @@ window.onload = function () {
 //         minutes = minutes < 60 ? "0" + minutes : minutes;
 //         seconds = seconds < 60 ? "0" + ":" + seconds : seconds;
         
-//         display.textContent = hours + ":" + minutes + ":" + seconds;
+        // display.textContent = hours + ":" + minutes + ":" + seconds;
 
 //         if (--timer < 0) {
 //             timer = duration;
@@ -92,3 +124,5 @@ window.onload = function () {
 //     timer(twoWeeks, display);
 //     timer.innerHTML = display
 // }
+
+// http request for google docs API
