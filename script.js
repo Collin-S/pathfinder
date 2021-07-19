b6733640ef505aabee01e97a521f7076516c6b59
Collin Sudik <Csudik@yahoo.com>
2021-07-19 18:18:27 -0400

    reformatted files

--- script.js
+++ script.js
@@ -1,139 +1,148 @@
 // dice roller
 function rollDice() {
-    return 1 + Math.floor(Math.random() * 20);
+  return 1 + Math.floor(Math.random() * 20);
 }
 
 function diceRoll(number) {
-    var dice = document.getElementById("dice-face");
-    dice.innerHTML = number;
+  var dice = document.getElementById("dice-face");
+  dice.innerHTML = number;
 }
 
 var button = document.getElementById("button");
 
-button.onclick = function() {
-    var result = rollDice();
-    diceRoll(result);
-}
+button.onclick = function () {
+  var result = rollDice();
+  diceRoll(result);
+};
 
 // Countdown timer
 function dateDifference() {
-    const now = new Date();
-    return Math.floor((then - now)/1000);
+  const now = new Date();
+  return Math.floor((then - now) / 1000);
 }
 
-const then =  getTargetDate();
+const then = getTargetDate();
 
 const addWeeks = (date, numWeeks) => {
+  const timestampInMilliseconds = date.getTime();
+  const millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
+  const millisecondsToAdd = numWeeks * millisecondsPerWeek;
+  return new Date(timestampInMilliseconds + millisecondsToAdd);
+};
+
+function getTargetDate() {
+  const addWeeks = (date, numWeeks) => {
     const timestampInMilliseconds = date.getTime();
     const millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
     const millisecondsToAdd = numWeeks * millisecondsPerWeek;
     return new Date(timestampInMilliseconds + millisecondsToAdd);
   };
 
-function getTargetDate() {
-    const addWeeks = (date, numWeeks) => {
-    const timestampInMilliseconds = date.getTime();
-    const millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
-    const millisecondsToAdd = numWeeks * millisecondsPerWeek;
-    return new Date(timestampInMilliseconds + millisecondsToAdd);
-    };
-
-    const baseline = new Date("5/30/2021 14:00");
-    let dateFound = false;
-    const now = new Date();
-    let targetDate = baseline;
-    while(!dateFound) {
-        targetDate = addWeeks(targetDate, 2)
-        if(targetDate - now > 0) {
-            dateFound = true;
-        }
+  const baseline = new Date("5/30/2021 14:00");
+  let dateFound = false;
+  const now = new Date();
+  let targetDate = baseline;
+  while (!dateFound) {
+    targetDate = addWeeks(targetDate, 2);
+    if (targetDate - now > 0) {
+      dateFound = true;
     }
-    return targetDate;
+  }
+  return targetDate;
 }
 
 function getDays(seconds) {
-    return Math.floor(seconds/86400);
+  return Math.floor(seconds / 86400);
 }
 
 function getHours(seconds) {
-    return Math.floor(seconds/3600);
+  return Math.floor(seconds / 3600);
 }
 
 function getMinutes(seconds) {
-    return Math.floor(seconds/60);
+  return Math.floor(seconds / 60);
 }
 
 function zeroPad(num) {
-    return (num < 10 ? "0" : "") + num;
+  return (num < 10 ? "0" : "") + num;
 }
 
 function tick() {
-    //The difference between the date and now
-    let difference = dateDifference();
-    const days = getDays(difference);
-    difference = difference - days * 86400;
-    const hours = getHours(difference);
-    difference = difference - hours * 3600;
-    const minutes = getMinutes(difference);
-    difference = difference - minutes * 60;
-    const seconds = difference;
+  //The difference between the date and now
+  let difference = dateDifference();
+  const days = getDays(difference);
+  difference = difference - days * 86400;
+  const hours = getHours(difference);
+  difference = difference - hours * 3600;
+  const minutes = getMinutes(difference);
+  difference = difference - minutes * 60;
+  const seconds = difference;
 
-    document.getElementById("timer").innerHTML = zeroPad(days) + ":" + zeroPad(hours) + ":" + zeroPad(minutes) + ":" + zeroPad(seconds);
+  document.getElementById("timer").innerHTML =
+    zeroPad(days) +
+    ":" +
+    zeroPad(hours) +
+    ":" +
+    zeroPad(minutes) +
+    ":" +
+    zeroPad(seconds);
 }
 
 window.onload = function () {
-    countdown = setInterval(tick, 1000);
-    tick();
-}
+  countdown = setInterval(tick, 1000);
+  tick();
+};
 
 // Contact Form
 var fields = {};
 
-document.addEventListener("DOMContentLoaded", function() {
-    fields.formName = document.getElementById('formName');
-    fields.formEmail = document.getElementById('formEmail');
-    fields.formMessage = document.getElementById('formMessage');
-})
+document.addEventListener("DOMContentLoaded", function () {
+  fields.formName = document.getElementById("formName");
+  fields.formEmail = document.getElementById("formEmail");
+  fields.formMessage = document.getElementById("formMessage");
+});
 
 // Checks to verify form fields are not empty
 function notEmpty(value) {
-    if (value == null || typeof value == 'undefined' ) return false;
-    return (value.length > 0);
+  if (value == null || typeof value == "undefined") return false;
+  return value.length > 0;
 }
 
 function isName(text) {
-    const regex = /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/;
-    return regex.test(String(text).toLowerCase());
+  const regex =
+    /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/;
+  return regex.test(String(text).toLowerCase());
 }
 
 function isEmail(email) {
-    const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    return regex.test(String(email).toLowerCase());
+  const regex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  return regex.test(String(email).toLowerCase());
 }
 
-const form = document.getElementById('contactUs');
+const form = document.getElementById("contactUs");
 if (form) {
-  form.addEventListener('submit', (event) => {
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const name = document.getElementById('formName').value;
-    const email = document.getElementById('formEmail').value;
-    const message = document.getElementById('formMessage').value;
-	if(isEmail(email) && isName(name)) {
-      fetch('http://localhost:8080/post-form', {
-        method: 'POST',
+    
+    const email = document.getElementById("formEmail").value;
+    const message = document.getElementById("formMessage").value;const name = document.getElementById("formName").value;
+    if (isEmail(email) && isName(name)) {
+      fetch("http://localhost:8080/post-form", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
+          "Content-Type": "application/json",
+          Accept: "application/json",
         },
         body: JSON.stringify({
           name: name,
           email: email,
-          message: message
-        })
+          message: message,
+        }),
       });
     } else {
-        alert("Invalid name or email");
+      alert("Invalid name or email");
     }
   });
 }
--- server.js
+++ server.js
@@ -1,59 +1,65 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-const {parse} = require ('querystring');
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+const { parse } = require("querystring");
 
-http.createServer(function (request, response) {
-    console.log('Request: ', request.url);
-    var filePath = './' + request.url;
-    if (request.url === '/') {
-        filePath = './index.html';
+http
+  .createServer(function (request, response) {
+    console.log("Request: ", request.url);
+    var filePath = "./" + request.url;
+    if (request.url === "/") {
+      filePath = "./index.html";
     }
-    if (request.url === '/post-form') {
-        request.on('data', jsonData => {
-            const data = JSON.parse(jsonData.toString());
-            console.log(data);
-            // Save the data to a file.
-            response.writeHead(200);
-            response.end();
-        });
+    if (request.url === "/post-form") {
+      request.on("data", (jsonData) => {
+        const data = JSON.parse(jsonData.toString());
+        console.log(data);
+        // Save the data to a file.
+        
+
+        response.writeHead(200);
+        response.end();
+      });
     }
     var extname = String(path.extname(filePath)).toLowerCase();
     var mimeTypes = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
-        '.json': 'application/json',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.gif': 'image/gif',
-        '.svg': 'image/svg+xml',
-        '.wav': 'audio/wav',
-        '.mp4': 'video/mp4',
-        '.woff': 'application/font-woff',
-        '.ttf': 'application/font-ttf',
-        '.eot': 'application/vnd.ms-fontobject',
-        '.otf': 'application/font-otf',
-        '.wasm': 'application/wasm'
+      ".html": "text/html",
+      ".js": "text/javascript",
+      ".css": "text/css",
+      ".json": "application/json",
+      ".png": "image/png",
+      ".jpg": "image/jpg",
+      ".gif": "image/gif",
+      ".svg": "image/svg+xml",
+      ".wav": "audio/wav",
+      ".mp4": "video/mp4",
+      ".woff": "application/font-woff",
+      ".ttf": "application/font-ttf",
+      ".eot": "application/vnd.ms-fontobject",
+      ".otf": "application/font-otf",
+      ".wasm": "application/wasm",
     };
-    var contentType = mimeTypes[extname] || 'application/octet-stream';
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT') {
-                fs.readFile('./404.html', function(error, content) {
-                    response.writeHead(404, { 'Content-Type': 'text/html' });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-            }
+    var contentType = mimeTypes[extname] || "application/octet-stream";
+    fs.readFile(filePath, function (error, content) {
+      if (error) {
+        if (error.code == "ENOENT") {
+          fs.readFile("./404.html", function (error, content) {
+            response.writeHead(404, { "Content-Type": "text/html" });
+            response.end(content, "utf-8");
+          });
+        } else {
+          response.writeHead(500);
+          response.end(
+            "Sorry, check with the site admin for error: " +
+              error.code +
+              " ..\n"
+          );
         }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
+      } else {
+        response.writeHead(200, { "Content-Type": contentType });
+        response.end(content, "utf-8");
+      }
     });
-}).listen(8080);
-console.log('Server running at http://127.0.0.1:8080/');
+  })
+  .listen(8080);
+console.log("Server running at http://127.0.0.1:8080/");
